b5a2cd51b1173a248b0e803389d64f3313708978
Tharini Parthasarathy <thariniparthasarathy1804@gmail.com>
2026-05-27 20:16:17 +0530

    fix: remove unused Platform import in StudaiPanelProvider

--- app/Providers/Filament/StudaiPanelProvider.php
+++ app/Providers/Filament/StudaiPanelProvider.php
@@ -10,7 +10,6 @@ use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Support\Enums\Platform;
 use Filament\View\PanelsRenderHook;
 use Filament\Widgets\AccountWidget;
 use Filament\Widgets\FilamentInfoWidget;
